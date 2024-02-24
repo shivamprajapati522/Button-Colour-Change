c6ccb174cd390e373cdebf990e1784926d6dce29
SHIVAM PRAJAPATI <133947500+shivamprajapati522@users.noreply.github.com>
2024-02-25 01:06:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+var temp = document.querySelector('.btn')
+const colorchange = () => {
+    temp.classList.toggle("btn1");
+    // alert("hello")
+  
+  };
+  temp.addEventListener("click", () => colorchange());
